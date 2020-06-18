574de863113f31ac31a26c5b0b08df776aad2587
nzaouesgi <hakhenaton@protonmail.com>
2020-06-18 20:32:09 +0200

    added HTTP upload method

--- src/HttpClient.cpp
+++ src/HttpClient.cpp
@@ -9,6 +9,7 @@
 using namespace std;
 
 static size_t DownloadFile_WriteDataCallback(void *data, size_t size, size_t nmemb, void *userPtr);
+static size_t UploadFile_ReadDataCallback(void *ptr, size_t size, size_t nmemb, void *userPtr);
 
 HttpClient::HttpClient (){
     
@@ -34,6 +35,21 @@ static size_t DownloadFile_WriteDataCallback(void *data, size_t size, size_t nme
         return 0;
 
     return size * nmemb;
+}
+
+static size_t UploadFile_ReadDataCallback(void *data, size_t size, size_t nmemb, void *userPtr){
+
+    ifstream *inFile = (ifstream *)userPtr;
+
+    if (inFile == nullptr || !(*inFile).is_open())
+        return CURL_READFUNC_ABORT;
+
+    (*inFile).read((char *)data, size * nmemb);
+
+    if ((*inFile).bad() || ((*inFile).fail() && !(*inFile).eof()))
+        return CURL_READFUNC_ABORT;
+
+    return (*inFile).tellg();
 }
 
 bool HttpClient::DownloadFile(const string& url, const string& outputFile){
@@ -94,7 +110,7 @@ bool HttpClient::SendPostRequest(const std::string& url, const json& body){
     curl_easy_setopt(this->curl, CURLOPT_POST, 1L);
     curl_easy_setopt(this->curl, CURLOPT_COPYPOSTFIELDS, body.dump().c_str()); 
 
-    if(curl_easy_perform(this->curl) != CURLE_OK)
+    if (curl_easy_perform(this->curl) != CURLE_OK)
         goto clean;
 
     if (curl_easy_getinfo(this->curl, CURLINFO_RESPONSE_CODE, &code) != CURLE_OK || code < 200 || code >= 300)
@@ -114,16 +130,56 @@ clean:
  
 };
 
-bool UploadFile(const std::string& url, const std::string& fileName){
+bool HttpClient::UploadFile(const std::string& url, const std::string& fileName){
 
     bool ret = false;
+    ifstream inFile;
+    streampos fileSize = 0;
+    long code = -1L;
 
-    
+    if (this->curl == NULL)
+        goto clean;
 
+    inFile.open(fileName, ofstream::binary);
+
+    if (!inFile.is_open())
+        goto clean;
+
+    curl_easy_setopt(this->curl, CURLOPT_READFUNCTION, UploadFile_ReadDataCallback);
+    curl_easy_setopt(this->curl, CURLOPT_READDATA, (void *)&inFile);
+    curl_easy_setopt(this->curl, CURLOPT_UPLOAD, 1L);
+    curl_easy_setopt(this->curl, CURLOPT_URL, url);
+
+    inFile.seekg(0, ios_base::end);
+    if (inFile.bad() || inFile.fail())
+        goto clean;
+
+    fileSize = inFile.tellg();
+
+    if (fileSize == -1)
+        goto clean;
+
+    inFile.seekg(0);
+    if (inFile.bad() || inFile.fail())
+        goto clean;
+
+    curl_easy_setopt(this->curl, CURLOPT_INFILESIZE_LARGE, (curl_off_t)fileSize);
+
+    if (curl_easy_perform(this->curl) != CURLE_OK)
+        goto clean;
+
+    if (curl_easy_getinfo(this->curl, CURLINFO_RESPONSE_CODE, &code) != CURLE_OK || code < 200 || code >= 300)
+        goto clean;
 
     ret = true;
 
 clean:
+
+    if (inFile.is_open())
+        inFile.close();
+
+    if (this->curl != NULL)
+        curl_easy_reset(this->curl);
 
     return ret;
 };
