c39be86bec46ca636fe01016c22810ecc3c2efea
nzaouesgi <hakhenaton@protonmail.com>
2020-06-18 19:27:06 +0200

    added huffman as process

--- src/CompressionProcess.cpp
+++ src/CompressionProcess.cpp
@@ -1,9 +1,19 @@
 #include "CompressionProcess.h"
+#include "Huffman.h"
 
 CompressionProcess::CompressionProcess(ProcessType alg){
     this->alg = alg;
 }
 
 bool CompressionProcess::Execute(string& filePath, string& outputPath){
-    return filePath == "nullptr" && outputPath == "nullptr";
+    switch (this->alg){
+        case ProcessType::COMPRESS_HUFFMAN:
+            Huffman h;
+            return h.CompressFile(filePath, outputPath); 
+            break;
+        case ProcessType::DECOMPRESS_HUFFMAN:
+            Huffman h;
+            return h.DecompressFile(filePath, outputPath); 
+            break;
+    }
 }
